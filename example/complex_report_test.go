6e95767bd2294e804f853a40dfbf951d7b52b326
zhanghaiyang <1125694337@qq.com>
2022-10-17 23:04:38 +0800

    init project

---
+++ example/complex_report_test.go
@@ -0,0 +1,215 @@
+package example
+
+import (
+	"fmt"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/zhyocean/gopdf"
+	"github.com/zhyocean/gopdf/core"
+)
+
+const (
+	ErrFile    = 1
+	FONT_MY    = "微软雅黑"
+	FONT_MD    = "MPBOLD"
+	DateFormat = "2006-01-02 15:04:05"
+)
+
+var (
+	largeFont = core.Font{Family: FONT_MY, Size: 15}
+	headFont  = core.Font{Family: FONT_MY, Size: 12}
+	textFont  = core.Font{Family: FONT_MY, Size: 10}
+)
+
+func ComplexReport() {
+	r := core.CreateReport()
+	font1 := core.FontMap{
+		FontName: FONT_MY,
+		FileName: "ttf//microsoft.ttf",
+	}
+	font2 := core.FontMap{
+		FontName: FONT_MD,
+		FileName: "ttf//mplus-1p-bold.ttf",
+	}
+	r.SetFonts([]*core.FontMap{&font1, &font2})
+	r.SetPage("A4", "P")
+	r.FisrtPageNeedHeader = true
+	r.FisrtPageNeedFooter = true
+
+	r.RegisterExecutor(core.Executor(ComplexReportExecutor), core.Detail)
+	r.RegisterExecutor(core.Executor(ComplexReportFooterExecutor), core.Footer)
+	r.RegisterExecutor(core.Executor(ComplexReportHeaderExecutor), core.Header)
+
+	r.Execute(fmt.Sprintf("complex_report_test.pdf"))
+	r.SaveAtomicCellText("complex_report_test.txt")
+}
+
+func ComplexReportExecutor(report *core.Report) {
+	var (
+		data      ReportDetail
+		lineSpace = 1.0
+		lineHight = 16.0
+	)
+
+	ret, errStr := getReportDetailData(&data)
+	if ret != 0 {
+		panic(struct {
+			ret    int
+			errStr string
+		}{ret: ret, errStr: errStr})
+	}
+
+	dir, _ := filepath.Abs("pictures")
+	qrcodeFile := fmt.Sprintf("%v/qrcode.png", dir)
+	line := gopdf.NewHLine(report).SetMargin(core.Scope{Top: 3.0, Bottom: 6.8}).SetWidth(0.1)
+	// todo: 任务详情
+	div := gopdf.NewDivWithWidth(100, lineHight, lineSpace, report)
+	div.SetFont(largeFont)
+	div.SetContent("测试报告").GenerateAtomicCell()
+	line.GenerateAtomicCell()
+
+	// 二维码
+	im := gopdf.NewImageWithWidthAndHeight(qrcodeFile, 70, 70, report)
+	im.SetMargin(core.Scope{Left: 340, Top: -40})
+	im.SetAutoBreak()
+	im.GenerateAtomicCell()
+
+	// 基本信息
+	report.SetMargin(10, -25)
+	baseInfoDiv := gopdf.NewDivWithWidth(100, lineHight, lineSpace, report)
+	baseInfoDiv.SetFont(headFont)
+	baseInfoDiv.SetContent("报告概要").GenerateAtomicCell()
+
+	baseInfo := gopdf.NewDivWithWidth(200, lineHight, lineSpace, report)
+	baseInfo.SetMarign(core.Scope{Left: 12, Top: 3.0})
+	baseInfo.SetFont(textFont).SetContent(fmt.Sprintf("任务: %s", data.JobName)).GenerateAtomicCell()
+	baseInfo.Copy(fmt.Sprintf("创建人: %s", data.CreatUserName)).GenerateAtomicCell()
+	baseInfo.Copy(fmt.Sprintf("状态: %s", data.Status)).GenerateAtomicCell()
+	baseInfo.Copy(fmt.Sprintf("类别: %s", data.IssueClassName)).GenerateAtomicCell()
+
+	// 模板
+	report.SetMargin(6, 3)
+	baseInfoDiv.Copy("详细过程").GenerateAtomicCell()
+	report.SetMargin(0, 3)
+
+	// todo: 评论
+	report.SetMargin(0, 10)
+	div.Copy("评论信息").GenerateAtomicCell()
+	line.GenerateAtomicCell()
+
+	if len(data.Contents) == 0 {
+		nodataDiv := gopdf.NewDivWithWidth(150, lineHight, lineSpace, report)
+		nodataDiv.SetFont(textFont).SetContent("\t没有回复记录").GenerateAtomicCell()
+		report.SetMargin(0, 3)
+	}
+	for _, content := range data.Contents {
+		cellStr := fmt.Sprintf("\t%s    %s    %s", content.Time, content.Msg, content.CreateUser)
+		comment := gopdf.NewDivWithWidth(415, lineHight, lineSpace, report)
+		comment.SetFont(textFont).SetContent(cellStr).GenerateAtomicCell()
+		report.SetMargin(0, 3)
+	}
+
+	// todo: 历史记录
+	report.SetMargin(0, 3)
+	historyDiv := div.Copy("历史回复")
+	historyDiv.GenerateAtomicCell()
+	line.GenerateAtomicCell()
+
+	if len(data.History) == 0 {
+		nodataDiv := gopdf.NewDivWithWidth(150, lineHight, lineSpace, report)
+		nodataDiv.SetFont(textFont).SetContent("\t没有历史记录").GenerateAtomicCell()
+		report.SetMargin(0, 3)
+	}
+
+	for _, content := range data.History {
+		cellStr := fmt.Sprintf("\t%s    %s    %s", content.Time, content.Msg, content.CreateUser)
+		comment := gopdf.NewDivWithWidth(415, lineHight, lineSpace, report)
+		comment.SetFont(textFont).SetContent(cellStr).GenerateAtomicCell()
+		report.SetMargin(0, 3)
+	}
+}
+func ComplexReportFooterExecutor(report *core.Report) {
+	content := fmt.Sprintf("第 %v / {#TotalPage#} 页", report.GetCurrentPageNo())
+	footer := gopdf.NewSpan(10, 0, report)
+	footer.SetFont(textFont)
+	footer.SetFontColor("60, 179, 113")
+	footer.HorizontalCentered().SetContent(content).GenerateAtomicCell()
+}
+func ComplexReportHeaderExecutor(report *core.Report) {
+	content := "github.com/tiechui1994/gopdf"
+	footer := gopdf.NewSpan(10, 0, report)
+	footer.SetFont(textFont)
+	footer.SetFontColor("255,0,0")
+	footer.SetBorder(core.Scope{Top: 10})
+	footer.HorizontalCentered().SetContent(content).GenerateAtomicCell()
+}
+
+type ExportInfo struct {
+	Time       string
+	Msg        string
+	CreateUser string
+}
+
+type ReportDetail struct {
+	JobName           string
+	CreatedAt         string
+	CreatUserName     string
+	Status            string
+	IssueClassName    string
+	IssueSubClassName string
+	TimeOut           string
+
+	Template map[string]string
+
+	Contents []ExportInfo
+	History  []ExportInfo
+}
+
+func getReportDetailData(data *ReportDetail) (ret int, errStr string) {
+	data.JobName = "技术指导"
+	data.CreatedAt = time.Now().Format(DateFormat)
+	data.CreatUserName = "钱伟长"
+	data.Status = "已经完成"
+	data.IssueClassName = "发动机类别"
+	data.IssueSubClassName = "测试飞机发动机"
+
+	data.Contents = []ExportInfo{
+		{
+			time.Now().Format("2006-01-02 15:03:04"),
+			"涡扇发动机结构，涡扇发动机通俗的讲可以看做两根粗细不同的管子套在一起组成的。细管子里包含了低高压气机、燃烧室、低高压涡轮，最后连接至尾喷管，这根内管所包裹的空间叫做发动机的内涵道，流经里头的空气叫内涵气流。而套在内涵道外面的粗管子则包裹着风扇以及整个或者部分细管子（内涵道），我们需要注意。",
+			"钱学森",
+		},
+		{
+			time.Now().Format("2006-01-02 15:03:04"),
+			"涡扇发动机工作原理，发动机前方的风扇旋转吸入空气被分为两个部分，一部分进入细管子成为内涵气流，一部分进入粗管子成为外涵气流，外涵气流直接从发动机尾部流出形成一部分动力，而内涵气流经过压气机被压缩，成为高温高压气体，并进一步进入燃烧室被和燃油一起进一步加热膨胀冲击后面的涡轮，涡轮就像我们小时候玩的纸风车一样，被高温高压燃气带动旋转，燃气最后从尾部高速喷出，形成发动机最主要的动力。",
+			"冯卡门",
+		},
+		{
+			time.Now().Format("2006-01-02 15:03:04"),
+			"由于涡轮与风扇、压气机同在一根轴承，因此涡轮又带动了风扇、压气机一起转动。吸入空气—增压-加热-喷射—带动发动机运转，这样一个稳定的循环运动就初步建立起来，是不是看起来很像一个永动机，只要发动机一发动，就能持续的提供动力。",
+			"爱因斯坦",
+		},
+	}
+
+	data.History = []ExportInfo{
+		{
+			time.Now().Format("2006-01-02 15:03:04"),
+			"涡扇发动机启动，其实现代的飞机尾部一般会加装了一个辅助动力装置，叫做APU.它是涡扇发动机能够启动的关键所在，APU是由一个小电动机带动的小型" +
+				"燃气轮机，在APU启动后，就能源源不断的吸入空气并送到发动机后面的燃烧室燃烧然后带动整个涡扇发动机的运转，所以涡扇发动机启动前，必须先启动APU，如果" +
+				"APU故障了就只能依靠地面电源车和高压气源车来实现发动机的启动",
+			"邓稼先",
+		},
+		{
+			time.Now().Format("2006-01-02 15:03:04"),
+			"很好, 照办",
+			"毛泽东",
+		},
+	}
+	return ret, errStr
+}
+
+func TestJobExport(t *testing.T) {
+	ComplexReport()
+}
---
+++ image.go
@@ -0,0 +1,143 @@
+package gopdf
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+type Image struct {
+	pdf           *core.Report
+	autobreak     bool
+	path          string
+	width, height float64
+	margin        core.Scope
+	temppath      []string
+}
+
+func NewImage(path string, pdf *core.Report) *Image {
+	return NewImageWithWidthAndHeight(path, 0, 0, pdf)
+}
+
+func NewImageWithWidthAndHeight(path string, width, height float64, pdf *core.Report) *Image {
+	contentWidth, contentHeight := pdf.GetContentWidthAndHeight()
+	if width > contentWidth {
+		width = contentWidth
+	}
+	if height > contentHeight {
+		height = contentHeight
+	}
+
+	var temppath []string
+	if _, err := os.Stat(path); err != nil {
+		path = fmt.Sprintf("/tmp/%v.png", time.Now().Unix())
+		temppath = append(temppath, path)
+		DrawPNG(path)
+	}
+
+	dstPath := fmt.Sprintf("/tmp/%v.jpeg", time.Now().UnixNano())
+	srcPath, _ := filepath.Abs(path)
+	Convert2JPEG(srcPath, dstPath)
+	temppath = append(temppath, dstPath)
+
+	image := &Image{
+		pdf:      pdf,
+		path:     dstPath,
+		temppath: temppath,
+	}
+
+	image.imageWidthHeight(image.path, width, height)
+
+	if dstPath != "" {
+		pdf.AddCallBack(image.delTempImage)
+	}
+
+	return image
+}
+
+func (image *Image) imageWidthHeight(path string, width, height float64) *Image {
+	w, h := GetImageWidthAndHeight(path)
+	if width > 0 && height > 0 {
+		if float64(h)*width/float64(w) > height {
+			width = float64(w) * height / float64(h)
+		} else {
+			height = float64(h) * width / float64(w)
+		}
+	} else if width > 0 {
+		height = float64(h) * width / float64(w)
+	} else if height > 0 {
+		width = float64(w) * height / float64(h)
+	} else {
+		width, height = float64(w), float64(h)
+	}
+
+	image.width = width
+	image.height = height
+
+	return image
+}
+
+func (image *Image) SetMargin(margin core.Scope) *Image {
+	margin.ReplaceMarign()
+	image.margin = margin
+	return image
+}
+
+func (image *Image) GetHeight() float64 {
+	return image.height
+}
+func (image *Image) GetWidth() float64 {
+	return image.width
+}
+
+func (image *Image) SetAutoBreak() {
+	image.autobreak = true
+}
+
+// 自动换行
+func (image *Image) GenerateAtomicCell() (pagebreak, over bool, err error) {
+	var (
+		sx, sy = image.pdf.GetXY()
+	)
+
+	x, y := sx+image.margin.Left, sy+image.margin.Top
+	pageEndX, pageEndY := image.pdf.GetPageEndXY()
+	if y < pageEndY && y+float64(image.height) > pageEndY {
+		if image.autobreak {
+			image.pdf.AddNewPage(false)
+			goto draw
+		}
+
+		return true, false, nil
+	}
+
+draw:
+	image.pdf.Image(image.path, x, y, x+float64(image.width), y+float64(image.height))
+	if x+float64(image.width) >= pageEndX {
+		sx, _ = image.pdf.GetPageStartXY()
+		image.pdf.SetXY(sx, y+float64(image.height)+image.margin.Bottom)
+	} else {
+		image.pdf.SetXY(x+float64(image.width), y)
+	}
+
+	if image.autobreak {
+		sx, _ = image.pdf.GetPageStartXY()
+		_, sy := image.pdf.GetXY()
+		image.pdf.SetXY(sx, sy+image.height)
+	}
+
+	return false, true, nil
+}
+
+func (image *Image) delTempImage(report *core.Report) {
+	if image.temppath == nil {
+		return
+	}
+
+	for _, path := range image.temppath {
+		if _, err := os.Stat(path); err == nil || os.IsExist(err) {
+			os.Remove(path)
+		}
+	}
+}
